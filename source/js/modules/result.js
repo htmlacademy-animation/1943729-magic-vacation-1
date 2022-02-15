2a1e52873a8c6ed72b436c7b94c50003fe90df83
Aleksandr Racovets <57405165+alexracovets@users.noreply.github.com>
2022-02-15 02:20:42 +0200

    fixed module3-task4

--- source/js/modules/result.js
+++ source/js/modules/result.js
@@ -14,6 +14,7 @@ export default () => {
         });
         targetEl[0].classList.add(`screen--show`);
         targetEl[0].classList.remove(`screen--hidden`);
+        startAnimSlogan(targetEl[0]);
       });
     }
 
@@ -29,4 +30,103 @@ export default () => {
       });
     }
   }
+
+
+
+  const createSvgAnimationEl = (tag, attrs) => {
+    const el = document.createElementNS(`http://www.w3.org/2000/svg`, tag);
+
+    attrs.forEach((attr) => {
+      el.setAttribute(attr.name, attr.value);
+    });
+
+    return el;
+  };
+
+  const createDashAnim = (letter, length, index, svgFailSlogan) => {
+    const animate = createSvgAnimationEl(`animate`, [{
+        name: `attributeName`,
+        value: `stroke-dasharray`
+      },
+      {
+        name: `from`,
+        value: `0, ${length}`
+      },
+      {
+        name: `to`,
+        value: `${length}, 0`
+      },
+      {
+        name: `dur`,
+        value: `500ms`
+      },
+      {
+        name: `fill`,
+        value: `freeze`
+      }
+    ]);
+
+    letter.appendChild(animate);
+
+    if (svgFailSlogan) {
+      setTimeout(() => {
+        animate.beginElement();
+      }, 50 * (index + 1));
+    } else {
+      animate.beginElement();
+    }
+  };
+
+  const createOpacityAnim = (letter, length, index) => {
+    const animate = createSvgAnimationEl(`animate`, [{
+        name: `attributeName`,
+        value: `opacity`
+      },
+      {
+        name: `from`,
+        value: `0`
+      },
+      {
+        name: `to`,
+        value: `1`
+      },
+      {
+        name: `dur`,
+        value: `1ms`
+      },
+      {
+        name: `fill`,
+        value: `freeze`
+      }
+    ]);
+
+    setTimeout(() => {
+      letter.appendChild(animate);
+      letter.classList.add(`start-translate`);
+      animate.beginElement();
+    }, 80 * (index + 1));
+  };
+
+  const startAnimSlogan = (element) => {
+    const svgSlogan = element.querySelector(`.dash`);
+    const svgFailSlogan = element.getAttribute(`id`) === `result3`;
+    const letters = svgSlogan.querySelectorAll(`path`);
+
+    letters.forEach((letter, index) => {
+      const pathLength = letter.getTotalLength();
+      const length = pathLength / 3;
+      letter.setAttribute(`stroke-dasharray`, `0, ${length}`);
+
+      if (svgFailSlogan) {
+        setTimeout(() => {
+          createDashAnim(letter, length, index, svgFailSlogan);
+          createOpacityAnim(letter, length, index);
+        }, 500);
+      } else {
+        createDashAnim(letter, length);
+      }
+    });
+  };
+
+
 };
